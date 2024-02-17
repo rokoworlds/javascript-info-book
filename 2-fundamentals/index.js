c9a25a651a9540bb62d2deed0e5254c55c1c4898
Roman Coropat <roman.koropat@gmail.com>
2024-02-18 00:05:44 +0200

    add: 2.fundamentals: 2.13

--- 2-fundamentals/index.js
+++ 2-fundamentals/index.js
@@ -161,23 +161,46 @@ JS приобразует строку "0" правдивому значению
 // } while (num < 100 && num)
 
 // Задача 9
-const n = 10;
-next:
-for (let i = 2; i <= n; i++) {
+// const n = 10;
+// next:
+// for (let i = 2; i <= n; i++) {
 
-    for (let j = 2; j < i; j++) {
-        if (i % j === 0) continue next;
-    }
-    console.log(i);
-}
+//     for (let j = 2; j < i; j++) {
+//         if (i % j === 0) continue next;
+//     }
+//     console.log(i);
+// }
 
 
-nextPrime:
-for (let i = 2; i <= n; i++) { // Для всех i...
+// 2.13 Конструкция 'switch'
+// Задача 1
+// const browser = 'Safari';
 
-  for (let j = 2; j < i; j++) { // проверить, делится ли число..
-    if (i % j == 0) continue nextPrime; // не подходит, берём следующее
-  }
+// if (browser === 'Edge') {
+//     alert("You've got the Edge!");
+// } else if (
+//     browser === 'Chrome' 
+//     || browser === 'Firefox' 
+//     || browser === 'Safari' 
+//     || browser === 'Opera'
+// ) {
+//     alert('Okay we support these browsers too')
+// } else {
+//     alert('We hope that this page looks ok!')
+// }
 
-  alert( i ); // простое число
+// Задача 2
+const number = +prompt('Введите число между 0 и 3', '');
+
+switch (number) {
+    case 0:
+        alert('Вы ввели число 0');
+        break;
+    case 1:
+        alert('Вы ввели число 1');
+        break;
+    case 2:
+    case 3:
+        alert('Вы ввели число 2, а может и 3');
+        break;
 }
