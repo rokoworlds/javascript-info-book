df6df709c4335335ef7f6077f7665fed7fbf3dbf
Roman Coropat <roman.koropat@gmail.com>
2024-03-26 22:11:52 +0200

    add: deep cloning problem

--- 4-object-basics/index.js
+++ 4-object-basics/index.js
@@ -69,39 +69,119 @@
 
 // 4.4 Методы объекта, this
 // Задача 1
-function makeUser() {
-    return {
-        name: 'John',
-        ref: this,
-    };
-}
+// function makeUser() {
+//     return {
+//         name: 'John',
+//         ref: this,
+//     };
+// }
 
-let user = makeUser();
+// let user = makeUser();
 
-alert(user.ref.name);
+// alert(user.ref.name);
 
 // результатом будет ошибка, потому что в данном случае this будет равно undefined
 
 // Задача 2
 
-let calculator = {
-    a: 0,
-    b: 0,
-    res: 0,
-    read() {
-        this.a = +(prompt('a?'));
-        this.b = +(prompt('b?'));
-        return this
-    },
-    sum() {
-        return this.a + this.b;
-    },
-    mul() {
-        return this.a * this.b
-    }
+// let calculator = {
+//     a: 0,
+//     b: 0,
+//     res: 0,
+//     read() {
+//         this.a = +(prompt('a?'));
+//         this.b = +(prompt('b?'));
+//         return this
+//     },
+//     sum() {
+//         return this.a + this.b;
+//     },
+//     mul() {
+//         return this.a * this.b
+//     }
 
+// }
+
+// calculator.read();
+// alert(calculator.sum());
+// alert(calculator.mul());
+
+// Задача 3
+
+let ladder = {
+    step: 0,
+    up() {
+        this.step++;
+        return this;
+    },
+    down() {
+        this.step--;
+        return this;
+    },
+    showStep: function() {
+        alert(this.step);
+        return this;
+    }
+};
+
+// gpt problems
+
+// Вам необходимо реализовать функцию deepCopy,
+// которая принимает объект в качестве аргумента и 
+// возвращает его глубокую копию.
+
+const original = {
+    name: 'John',
+    age: 30,
+    address: {
+        city: 'New York',
+        country: 'USA'
+    }
+};
+
+// v1
+
+const deepCopy1 = (orig) => {
+    return structuredClone(orig);
 }
 
-calculator.read();
-alert(calculator.sum());
-alert(calculator.mul());
+// v2 
+
+const deepCopy2 = (orig) => {
+    return JSON.parse(JSON.stringify(orig))
+}
+
+// v3
+
+const deepCopy3 = (orig) => {
+    const copied = {};
+
+    for (let key in orig) {
+        if (orig[key] === 'object' && orig[key] !== null) {
+            copied[key] = deepCopy3(orig[key]);
+        } else {
+            copied[key] = orig[key];
+        }
+    }
+
+    return copied;
+}
+
+
+const copied = deepCopy3(original);
+
+
+console.log(copied);
+/* Ожидаемый результат:
+{
+    name: 'John',
+    age: 30,
+    address: {
+        city: 'New York',
+        country: 'USA'
+    }
+}
+*/
+
+console.log(original === copied); // false
+console.log(original.address === copied.address); // false
