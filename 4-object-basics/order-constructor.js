e9030e1a5ea9b57e674b058c1683fc4196d4f2a0
Roman Coropat <roman.koropat@gmail.com>
2024-04-12 18:20:19 +0300

    refactor: order changes

--- 4-object-basics/order-constructor.js
+++ 4-object-basics/order-constructor.js
@@ -22,46 +22,50 @@ function Order() {
 
     this.addItem = function(item, count) {
         //  добавить итем в чек (+ имя +цена)
-        if (!this.isLocked) {
-            let checkItem = {
-                name : item.name,
-                price: item.price,
-                count: count,
-            };
-            this.check.push(checkItem);
-            this.totalPrice += item.price * count;
-
-        } else {
+        if (this.isLocked) {
             console.log('Ошибка! Заказ заблокирован.');
+            return;
         }
+
+        let checkItem = {
+            name : item.name,
+            price: item.price,
+            count: count,
+        };
+
+        this.check.push(checkItem);
+        this.totalPrice += item.price * count;
+
     }
 
     this.removeItem = function(item, count) {
         // убрать из чека count итемов (если не указано сколько - убрать все). Нельзя убрать больше чем было в чеке
-        if (!this.isLocked) {
-            let index = this.check.findIndex(checkItem => checkItem.name === item.name);
-
-            if (index !== -1) {
-                let currentCount = this.check[index].count;
-    
-                if (count === undefined || count >= currentCount) {
-                    this.totalPrice -= this.check[index].price * currentCount;
-                    this.check.splice(index, 1);
-                } else {
-                    this.check[index].count -= count;
-                    this.totalPrice -= this.check[index].price * count;
-                }
-            } else {
-                console.log('Ошибка! Товар не найден в чеке.')
-            }
-        } else {
+        if (this.isLocked) {
             console.log('Ошибка! Заказ заблокирован.');
+            return;
+        };
+
+        const index = this.check.findIndex(checkItem => checkItem.name === item.name);
+
+        if (index === -1) {
+            console.log('Ошибка! Товар не найден в чеке.');
+            return;
+        };
+
+        let currentCount = this.check[index].count;
+
+        if (count === undefined || count >= currentCount) {
+            this.totalPrice -= this.check[index].price * currentCount;
+            this.check.splice(index, 1);
+        } else {
+            this.check[index].count -= count;
+            this.totalPrice -= this.check[index].price * count;
         }
     }
 
     this.getCheck = function() {
         //  получить информацию сколько каких итемов в чеке, общую цену, опционаольно цену за каждую позицию (за 3 пивка - 300р). Формат произвольный, чтобы был читабельный
-        
+        console.log(this.check);
     }
 
     this.lockOrder = function() {
