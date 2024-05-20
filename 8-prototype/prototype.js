b42a58eefb0c82c616bbad144cb96f5fac7d253f
Roman Coropat <roman.koropat@gmail.com>
2024-05-20 13:10:03 +0300

    add: 8.1

--- 8-prototype/prototype.js
+++ 8-prototype/prototype.js
@@ -44,3 +44,44 @@ let head = {
   bed.__proto__ = table;
   pockets.__proto__ = bed;
 
+// Задача 3
+
+let animal3 = {
+  eat() {
+    this.full = true;
+  }
+};
+
+let rabbit3 = {
+  __proto__: animal
+};
+
+rabbit3.eat();
+
+// свойство full получит rabbit
+
+
+// Задача 4
+
+let hamster = {
+  stomach: [],
+
+  eat(food) {
+    this.stomach = [food];
+  }
+};
+
+let speedy = {
+  __proto__: hamster
+};
+
+let lazy = {
+  __proto__: hamster
+};
+
+// Этот хомяк нашёл еду
+speedy.eat("apple");
+alert( speedy.stomach ); // apple
+
+// У этого хомяка тоже есть еда. Почему? Исправьте
+alert( lazy.stomach ); // apple
