8d233478a4caf1b606c6fb4f3f1599163f524f46
Roman Coropat <roman.koropat@gmail.com>
2024-05-29 22:56:46 +0300

    add: 11

---
+++ 11-promise/promise.js
@@ -0,0 +1,122 @@
+// 11.2 Promise basics
+
+// Задача 1
+
+let promise = new Promise(function(resolve, reject) {
+    resolve(1);
+  
+    setTimeout(() => resolve(2), 1000);
+  });
+  
+  promise.then(alert);
+
+// Задача 2
+
+function delay(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+  
+  delay(3000).then(() => alert('выполнилось через 3 секунды'));
+
+// Задача 3
+
+
+showCircle(150, 150, 100).then(div => {
+    div.classList.add('message-ball');
+    div.append("Hello, world!");
+  });
+
+
+  // 11.3 Цепочки промисов
+
+
+  // 11.4 Обработка ошибок
+
+  new Promise(function(resolve, reject) {
+    setTimeout(() => {
+      throw new Error("Whoops!");
+    }, 1000);
+  }).catch(alert);
+
+
+  // 11.8 async / await
+
+// Задача 1
+
+async function loadJson(url) { // (1)
+    let response = await fetch(url); // (2)
+  
+    if (response.status == 200) {
+      let json = await response.json(); // (3)
+      return json;
+    }
+  
+    throw new Error(response.status);
+  }
+  
+  loadJson('no-such-user.json')
+    .catch(alert); // Error: 404 (4)
+
+
+// Задача 2
+
+
+class HttpError extends Error {
+    constructor(response) {
+      super(`${response.status} for ${response.url}`);
+      this.name = 'HttpError';
+      this.response = response;
+    }
+  }
+  
+  async function loadJson(url) {
+    let response = await fetch(url);
+    if (response.status == 200) {
+      return response.json();
+    } else {
+      throw new HttpError(response);
+    }
+  }
+  
+  // Запрашивать логин, пока github не вернёт существующего пользователя.
+  async function demoGithubUser() {
+  
+    let user;
+    while(true) {
+      let name = prompt("Введите логин?", "iliakan");
+  
+      try {
+        user = await loadJson(`https://api.github.com/users/${name}`);
+        break; // ошибок не было, выходим из цикла
+      } catch(err) {
+        if (err instanceof HttpError && err.response.status == 404) {
+          // после alert начнётся новая итерация цикла
+          alert("Такого пользователя не существует, пожалуйста, повторите ввод.");
+        } else {
+          // неизвестная ошибка, пробрасываем её
+          throw err;
+        }
+      }
+    }
+  
+  
+    alert(`Полное имя: ${user.name}.`);
+    return user;
+  }
+  
+  demoGithubUser();
+
+// Задача 3
+
+async function wait() {
+    await new Promise(resolve => setTimeout(resolve, 1000));
+  
+    return 10;
+  }
+  
+  function f() {
+    // покажет 10 через 1 секунду
+    wait().then(result => alert(result));
+  }
+  
+  f();
